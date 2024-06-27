fc294f64ecd5ad77c6bc266efef8918a35853614
prashant-kumar96 <prashant.kumar560@gmail.com>
2024-06-27 12:44:39 +0530

    latest code

---
+++ myapp2/src/app/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+const HomePage = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/test");
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const result = await response.json();
+        console.log(result);
+        setData(result.message);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+  return (
+    <div className="h-screen bg-blue-500">
+      HomePage
+      {data}
+    </div>
+  );
+};
+
+export default HomePage;
